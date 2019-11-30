f5ed09a52e27f5612c1432bbaad4f83e3b389f94
Wallace Carr <commandercarr1@gmail.com>
2019-11-30 14:09:52 -0700

    update

--- requestEndpointBehavior.js
+++ requestEndpointBehavior.js
@@ -16,11 +16,28 @@ exports.initializeWebscrape = async () => {
 }
 
 exports.getStockDetailsWebscrapeTwi = async (link) => {
+    // console.log(`Going to the Product Page.. ( ${link} )`);
+    // await page.goto(link, {
+    //     waitUntil: 'networkidle0',
+    //   });
+    // await page.waitFor(5);
+    // let details = await page.evaluate(() => {
+    //     console.log('test');
+    //     console.log('cool');
+    //     var markup = document.documentElement.innerHTML;
+    //     return {
+    //         markup
+    //     }
+    // });
     console.log(`Going to the Product Page.. ( ${link} )`);
-    await page.goto(link, {
-        waitUntil: 'networkidle0',
-      });
-    await page.waitFor(5);
+    // await page.goto(link, {
+    //     waitUntil: 'networkidle0',
+    //   });
+    await page.goto(link, {waitUntil: 'domcontentloaded'});
+    // await page.waitFor(10);
+    await sleep(10000)
+    
+    console.log("Hello mr b sauce")
     let details = await page.evaluate(() => {
         console.log('test');
         console.log('cool');
@@ -34,4 +51,10 @@ exports.getStockDetailsWebscrapeTwi = async (link) => {
 
 exports.endWebscrape = async () => {
     await browser.close();
+}
+
+sleep = function(ms){
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
 }
